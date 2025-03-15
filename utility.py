1f45fa9473a0b506680a8328d1d15715fdc17ab7
sjf1616 <sjf16s20@gmail.com>
2025-03-14 22:05:26 -0300

    improvement in the core

--- utility.py
+++ utility.py
@@ -1,29 +1,33 @@
 def insert_sql():
-    import csv
-    with open("prov_sql.csv", newline="", encoding="utf-8") as arquivo:
-        sql_quoter = csv.DictWriter(arquivo)
-        ins_category = str(input('Write a category: ')).lower().strip()
-        ins_quoter = str(input('Write a quoter: '))
-        sql_quoter['category'] = ins_category
-        sql_quoter['quoter'] = ins_quoter
+    import csv #import library csv to manipulation
 
+    fieldnames = ['category', 'quoter'] #We defined the header of sql
 
-def choice_quoter():
-    import csv
-    from random import choice
-    with open("prov_sql.csv", newline="", encoding="utf-8") as arquivo:
-        #Transforma os dados dentro do .csv em dict
-        sql_quoter = csv.DictReader(arquivo) 
-
-        #Pede ao usuário inserir uma categoria para busca
-        category_input = str(input('Choice your category: ')).lower().strip()
-
-        if sql_quoter:
-            choice_list = [x for x in sql_quoter if x['category'] == category_input]
-            selected_quoter = choice(choice_list)
-            print(f'Frase escolhida: {selected_quoter['quoter']}')
+    with open("prov_sql.csv", 'a', newline="", encoding="utf-8") as arquivo: #open sql in mode append
+        sql_quoter = csv.DictWriter(arquivo, fieldnames=fieldnames) #defined content in sql as dictionary
+        if arquivo.tell() == 0: #check if row in sql not is empty
+            sql_quoter.writeheader() 
         
-        else:
+        ins_category = str(input('Write a category: ')).lower().strip() #input the category
+        ins_quoter = str(input('Write a quoter: ')) #input the quoter
+        sql_quoter.writerow({'category':ins_category, 'quoter':ins_quoter}) #write in a row the input
+        
+def choice_quoter():
+    import csv #import library csv to manipulation
+    from random import choice #import module choice from library random
+
+    with open("prov_sql.csv", newline="", encoding="utf-8") as arquivo: #open sql in mode read
+        
+        sql_quoter = csv.DictReader(arquivo) #defined content in sql as dictionary
+
+        category_input = str(input('Choice your category: ')).lower().strip() #input the category
+
+        if sql_quoter: #check if sql_quoter not is empty
+            choice_list = [x for x in sql_quoter if x['category'] == category_input] #assignment the value from sql_quoter if colum category equals category_input
+            selected_quoter = choice(choice_list) #choice any value in choice_list and assignment in variable
+            print(f'Frase escolhida: {selected_quoter['quoter']}') #print in screen the value choice
+        
+        else: #Case sql is empty return a error
             print(f'Error 0001 - Database not found')
         print()
 
@@ -37,11 +41,11 @@ def color_gui(color_text):
     return colors.get(color_text, '\033[m')
 
 def list_category():
-     import csv
-     with open('prov_sql.csv', newline="", encoding="utf-8") as arquivo:
-        reader_sql = sorted(csv.DictReader(arquivo))
-        for enum, line in enumerate(reader_sql):
-            print(f'{enum + 1} - {line['category'].capitalize()}')
+     import csv #import library csv to manipulation
+     with open('prov_sql.csv', 'r', newline="", encoding="utf-8") as arquivo: ##open sql in mode read
+        reader_sql = csv.DictReader(arquivo) #defined content in sql as dictionary
+        for enum, line in enumerate(reader_sql): #loop in len of reader_sql dictionary
+            print(f'{enum + 1} - {line['category'].capitalize()}') #print each category in sql_reader with first letter upper
         print()
         
 
